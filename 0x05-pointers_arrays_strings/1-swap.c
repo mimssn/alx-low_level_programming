3a51e5a37e2045cadbdf854fbac989a322d8d70a
mimssn <mohanedseedhamed@gmail.com>
2023-09-18 16:44:28 -0700

    a

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int-hjc
+ *@a: input parameter 1
+ *@b: input parameter 2
+ *Return: Nothing
+*/
+
+void swap_int (int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
