2b3c2c1d8370ec1f6f1ac12ddcacd15914f831d5
mimssn <mohanedseedhamed@gmail.com>
2023-09-25 17:20:53 -0700

    one

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,14 @@
  *  Return: copied memory with n byte changed
 */
 
-char *_memcpy(char *mar, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
 	int i = n;
 
 	for (; r < i; r++)
 	{
-		mar[r] - src[r];
+		dest[r] - src[r];
 		n--;
 	}
 	return (mar);
