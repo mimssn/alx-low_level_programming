5c3ec24af34b162163239d4a66a481147b582c4e
mimssn <mohanedseedhamed@gmail.com>
2023-09-28 16:11:05 -0700

    a

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,11 +2,6 @@
 #define MAIN_H
 
 #include <unistd.h>
-/**
-*void prototypes(void)
-*int prototypes(int)
-*char prototypes(char) 
-*/
 void _puts_recursion(char *s);
  void _print_rev_recursion(char *s);
 int _putchar(char c);
